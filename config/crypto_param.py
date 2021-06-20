36178c3136fc3a83cb398d4264da47a13db56688
zbluezstarz <zbluezstarz@gmail.com>
2021-06-21 00:52:02 +0900

    Modify logger msg of crypto_param

--- config/crypto_param.py
+++ config/crypto_param.py
@@ -2,7 +2,7 @@ from log.logging_api import *
 
 def get_crypto_parameter():
     logger.info("Get Exchange : Upbit")
-    logger.info("Get Strategy : 변동성 돌파 전략")
+    logger.info("Get Strategy : VolatilityBreakout")
     logger.info("Get Trading(Real or Backtest) : Real")
 
 
