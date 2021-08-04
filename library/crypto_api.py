9ef0b54e9629a512531141a45c38779f93bb69c7
zbluezstarz <zbluezstarz@gmail.com>
2021-08-05 00:12:48 +0900

    Modify get_custom_1days_ohlcv and Add create_backtest_data function

--- library/crypto_api.py
+++ library/crypto_api.py
@@ -75,28 +75,14 @@ def get_custom_1days_ohlcv(quotation_api, ticker, end_day_str, end_hours_int, da
     to_str = datetime.datetime.strptime(end_day_str, '%Y%m%d') + datetime.timedelta(hours=end_hours_int + 1)
     hours_unit = 24
     remain_day_num = day_num
-    df = quotation_api.get_ohlcv(ticker, interval="minute60", count=1 * hours_unit, to=to_str)
+    df = quotation_api.get_ohlcv(ticker, interval="minute60", count=day_num * hours_unit, to=to_str)
     remain_day_num -= 1
-    to_str -= datetime.timedelta(days=1)
-
-    while remain_day_num > 0:
-        print(remain_day_num)
-        new_1day_df = quotation_api.get_ohlcv(ticker, interval="minute60", count=1 * hours_unit, to=to_str)
-        if new_1day_df is None:
-            continue
-        if new_1day_df.shape[0] % 24 != 0:
-            print("== sample is shortage")
-            return None, None
-        df = pandas.concat([new_1day_df, df])
-        remain_day_num -= 1
-        to_str -= datetime.timedelta(days=1)
-        time.sleep(0.1)
 
     index = []
     data = []
 
     if ((day_num * hours_unit) > df.shape[0]) or df.shape[0] % 24 != 0:
-        print("** sample is shortage")
+        logger.critical("** sample is shortage")
         return None, None
 
     for i in range(day_num):
@@ -118,7 +104,75 @@ def get_custom_1days_ohlcv(quotation_api, ticker, end_day_str, end_hours_int, da
             'pro_half1': (df['close'][idx_end-int(hours_unit / 2) - 1] - df['open'][idx_start]) /
              df['open'][idx_start] * 100.0,
             'pro_half2': (df['close'][idx_end-1] - df['close'][idx_end-int(hours_unit / 2) - 1]) /
-             df['open'][idx_start] * 100.0
+             df['open'][idx_start] * 100.0,
+            'noise_ratio': 1.0 - ((abs(df['open'][idx_start] - df['close'][idx_end-1]) /
+                            (max(list((df['high'][idx_start:idx_end]).values)) - min(list((df['low'][idx_start:idx_end]).values))))),
+            'nr_half1': 1.0 - ((abs(df['open'][idx_start] - df['close'][idx_start + int(hours_unit/2) - 1]) /
+                            (max(list((df['high'][idx_start:idx_start + int(hours_unit/2)]).values)) - min(list((df['low'][idx_start:idx_start + int(hours_unit/2)]).values))))),
+            'nr_half2': 1.0 - ((abs(df['open'][idx_start + int(hours_unit/2)] - df['close'][idx_end-1]) /
+                            (max(list((df['high'][idx_start + int(hours_unit/2):idx_end]).values)) - min(list((df['low'][idx_start + int(hours_unit/2):idx_end]).values))))),
+
+            }
+        )
+        index.append(df.index[idx_end-1])
+    new_df = pandas.DataFrame(data, index=index)
+    return df, new_df
+
+
+def create_backtest_data_set(quotation_api, ticker, end_day_str, end_hours_int, day_num):
+    to_str = datetime.datetime.strptime(end_day_str, '%Y%m%d') + datetime.timedelta(hours=end_hours_int + 1)
+    hours_unit = 24
+    remain_day_num = day_num
+    df = quotation_api.get_ohlcv(ticker, interval="minute60", count=1 * hours_unit, to=to_str)
+    remain_day_num -= 1
+    to_str -= datetime.timedelta(days=1)
+
+    while remain_day_num > 0:
+        print(remain_day_num)
+        new_1day_df = quotation_api.get_ohlcv(ticker, interval="minute60", count=1 * hours_unit, to=to_str)
+        if new_1day_df is None:
+            continue
+        if new_1day_df.shape[0] % 24 != 0:
+            logger.critical("== sample is shortage")
+            return None, None
+        df = pandas.concat([new_1day_df, df])
+        remain_day_num -= 1
+        to_str -= datetime.timedelta(days=1)
+        time.sleep(0.1)
+
+    index = []
+    data = []
+
+    if ((day_num * hours_unit) > df.shape[0]) or df.shape[0] % 24 != 0:
+        logger.critical("** sample is shortage")
+        return None, None
+
+    for i in range(day_num):
+        idx_start = i * hours_unit
+        idx_end = (i+1) * hours_unit
+
+        data.append({
+            'open': df['open'][idx_start],
+            'high': max(list((df['high'][idx_start:idx_end]).values)),
+            'low': min(list((df['low'][idx_start:idx_end]).values)),
+            'close': df['close'][idx_end-1],
+            'volume': sum(list((df['volume'][idx_start:idx_end]).values)),
+            'value': sum(list((df['value'][idx_start:idx_end]).values)),
+            'vol_half1': sum(list((df['volume'][idx_start:idx_start+int(hours_unit/2)]).values)),
+            'vol_half2': sum(list((df['volume'][idx_start + int(hours_unit/2):idx_end]).values)),
+            'val_half1': sum(list((df['value'][idx_start:idx_start + int(hours_unit / 2)]).values)),
+            'val_half2': sum(list((df['value'][idx_start + int(hours_unit / 2):idx_end]).values)),
+            'profit': (df['close'][idx_end-1] - df['open'][idx_start]) / df['open'][idx_start] * 100.0,
+            'pro_half1': (df['close'][idx_end-int(hours_unit / 2) - 1] - df['open'][idx_start]) /
+             df['open'][idx_start] * 100.0,
+            'pro_half2': (df['close'][idx_end-1] - df['close'][idx_end-int(hours_unit / 2) - 1]) /
+             df['open'][idx_start] * 100.0,
+            'noise_ratio': 1.0 - ((abs(df['open'][idx_start] - df['close'][idx_end-1]) /
+                            (max(list((df['high'][idx_start:idx_end]).values)) - min(list((df['low'][idx_start:idx_end]).values))))),
+            'nr_half1': 1.0 - ((abs(df['open'][idx_start] - df['close'][idx_start + int(hours_unit/2) - 1]) /
+                            (max(list((df['high'][idx_start:idx_start + int(hours_unit/2)]).values)) - min(list((df['low'][idx_start:idx_start + int(hours_unit/2)]).values))))),
+            'nr_half2': 1.0 - ((abs(df['open'][idx_start + int(hours_unit/2)] - df['close'][idx_end-1]) /
+                            (max(list((df['high'][idx_start + int(hours_unit/2):idx_end]).values)) - min(list((df['low'][idx_start + int(hours_unit/2):idx_end]).values))))),
 
             }
         )
