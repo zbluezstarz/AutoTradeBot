b1981971abb543c639af7e066dfb2cba262aba1d
zbluezstarz <zbluezstarz@gmail.com>
2021-08-18 06:16:14 +0900

    Add calculating mdd/hpr/ror/win_ratio/etc...

--- library/crypto_trade.py
+++ library/crypto_trade.py
@@ -1,7 +1,7 @@
 from config import crypto_param
 from config.crypto_param import *
 from library import crypto_api
-from strategy.volatility_breakout import *
+# from strategy.volatility_breakout import *
 from strategy.volatility_modi1 import *
 
 
@@ -26,9 +26,8 @@ class CryptoTrade:
         self.quotation_api = crypto_api.get_quotation_api(crypto_param.exchange)
 
         logger.info("Create Strategy Object")
-        self.current_strategy = \
-            VolatilityModi1(self.exchange_api, self.quotation_api)
-            # VolatilityBreakout(self.exchange_api, self.quotation_api)
+        self.current_strategy = VolatilityModi1(self.exchange_api, self.quotation_api)
+        # VolatilityBreakout(self.exchange_api, self.quotation_api)
 
         if crypto_param.exchange == "backtest":
             self.current_strategy.set_start_time(self.quotation_api.get_sim_start_time())
@@ -36,6 +35,20 @@ class CryptoTrade:
             self.init_back_index = self.quotation_api.get_sim_index_update()
             self.sim_day_num = self.quotation_api.get_sim_day_num()
             logger.debug("backtest simulation num " + str(self.sim_day_num))
+
+        self.cash = crypto_api.get_balance(self.exchange_api, "KRW")
+        self.start_cash = self.cash
+        self.highest_cash = self.cash
+        self.lowest_cash = self.cash
+        self.last_highest_cash = self.cash
+        self.last_lowest_cash = self.cash
+        self.ror = 1
+        self.mdd = 0
+        self.accumulate_ror = 1
+        self.trade_count = 0
+        self.win_count = 0
+        self.trade_logs = dict()
+        self.mdd_logs = dict()
 
         logger.info("Set Strategy Parameters")
         self.current_strategy.set_parameters(crypto_param)
@@ -60,6 +73,9 @@ class CryptoTrade:
         self.is_running = True
 
         self.is_no_action_time = False
+        previous_cash = self.cash
+
+        index = 0
 
         while self.is_running:
             try:
@@ -95,6 +111,7 @@ class CryptoTrade:
                     self.is_no_action_time = False
 
                     if crypto_param.exchange == "backtest":
+                        running_now = self.trade_count + 1
                         self.running_timing = True
                         self.restart_timing = False
                         self.quotation_api.set_sim_index_update()
@@ -103,10 +120,36 @@ class CryptoTrade:
                         else:
                             logger.info("Simulation Turn Restart")
 
-                    krw = crypto_api.get_balance(self.exchange_api, "KRW")
+                    self.cash = crypto_api.get_balance(self.exchange_api, "KRW")
+                    self.highest_cash = max(self.highest_cash, self.cash)
+                    self.lowest_cash = min(self.lowest_cash, self.cash)
+                    self.last_highest_cash = max(self.last_highest_cash, self.cash)
+                    self.last_lowest_cash = min(self.last_lowest_cash, self.cash)
 
-                    logger.info("****** " + str(krw) + " ******")
-                    # TODO : MDD logging
+                    dd = -1
+
+                    if self.last_highest_cash == self.cash:
+                        self.last_lowest_cash = self.cash
+
+                    if self.last_lowest_cash == self.cash:
+                        dd = (self.last_highest_cash - self.last_lowest_cash) / self.last_highest_cash * 100.0
+                        self.mdd = max(self.mdd, dd)
+
+                    if self.mdd == dd:
+                        self.mdd_logs[str(running_now)] = {'mdd': self.mdd, 'last_highest': self.last_highest_cash,
+                                                           'last_lowest': self.last_lowest_cash}
+                    self.trade_logs[str(running_now)] = {'profit_ratio': (self.ror - 1) * 100.0 if self.ror > 1 else 0,
+                                                         'loss_ratio': (self.ror - 1) * 100.0 if self.ror < 1 else 0}
+
+                    self.trade_count += 1
+                    if previous_cash < self.cash:
+                        self.win_count += 1
+                    self.ror = self.cash / previous_cash
+                    self.accumulate_ror *= self.ror
+
+                    logger.info("****** " + str(self.cash) + " ******")
+
+                    previous_cash = self.cash
 
                 else:
                     self.is_no_action_time = True
@@ -120,3 +163,23 @@ class CryptoTrade:
             time.sleep(self.transaction_time)
 
         logger.info("Stop " + self.current_strategy.name + " Trading")
+        logger.info("Trade Count : " + str(self.trade_count))
+        logger.info("Win Count : " + str(self.win_count) + ", " + str(self.win_count / self.trade_count * 100.0) + "%")
+        logger.info("ROR : " + str(self.ror))
+        logger.info("Accumulate ROR : " + str(self.accumulate_ror))
+        logger.info("Highest Cash : " + str(self.highest_cash))
+        logger.info("Lowest Cash : " + str(self.lowest_cash))
+        logger.info("MDD : " + str(self.mdd))
+
+        df = pandas.DataFrame.from_dict(self.trade_logs)
+        df = df.T
+        avg_profit = df[df['profit_ratio'] > 0]['profit_ratio'].mean()
+        avg_loss = df[df['loss_ratio'] < 0]['loss_ratio'].mean()
+
+        logger.info("HPR : " + str((self.cash - self.start_cash) / self.start_cash * 100.0))
+        logger.info("Avg Profit Ratio : " + str(avg_profit))
+        logger.info("Avg Loss Ratio : " + str(avg_loss))
+        logger.info("Profit Loss Ratio : " + str(abs(avg_profit / avg_loss)))
+
+        logger.debug(self.mdd_logs)
+        logger.debug(self.trade_logs)
