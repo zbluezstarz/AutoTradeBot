318e36b809c89f8a86059ccd005223990f6dde97
zbluezstarz <zbluezstarz@gmail.com>
2021-09-22 23:52:41 +0900

    start_trade 함수 내부 기능 별도 함수로 분할

--- library/crypto_trade.py
+++ library/crypto_trade.py
@@ -7,15 +7,6 @@ from strategy.volatility_modi1 import *
 
 class CryptoTrade:
     def __init__(self):
-        self.is_running = False
-        self.target_tickers = []
-        self.remain_buy_list = []
-        self.is_no_action_time = False
-        self.running_timing = False
-        self.restart_timing = False
-        self.count = 0
-        self.sim_day_num = 0  # Backtest Only
-
         logger.info("Get Crypto Class Parameter")
         crypto_param.print_crypto_parameter()
         self.transaction_time = float(crypto_param.transaction_sec)
@@ -29,7 +20,19 @@ class CryptoTrade:
         self.current_strategy = VolatilityModi1(self.exchange_api, self.quotation_api)
         # VolatilityBreakout(self.exchange_api, self.quotation_api)
 
-        if crypto_param.exchange == "backtest":
+        self.is_running = False
+        self.target_tickers = []
+        self.remain_buy_list = []
+        self.is_no_action_time = False
+        self.running_timing = False
+        self.restart_timing = False
+        self.count = 0
+        self.no_action_sleep_time = 300
+        self.sim_day_num = 0  # Backtest Only
+        self.backtest_index = 0
+        self.running_now = datetime.datetime.now()
+
+        if self.is_backtest():
             self.current_strategy.set_start_time(self.quotation_api.get_sim_start_time())
             self.running_timing = True
             self.init_back_index = self.quotation_api.get_sim_index_update()
@@ -40,6 +43,7 @@ class CryptoTrade:
         self.current_strategy.execute_turn_end_process()
 
         self.cash = crypto_api.get_balance(self.exchange_api, "KRW")
+        self.previous_cash = self.cash
         self.start_cash = self.cash
         self.highest_cash = self.cash
         self.lowest_cash = self.cash
@@ -55,6 +59,12 @@ class CryptoTrade:
 
         logger.info("Set Strategy Parameters")
         self.current_strategy.set_parameters(crypto_param)
+
+    def is_backtest(self):
+        if crypto_param.exchange == "backtest":
+            return True
+        else:
+            return False
 
     def init_trade(self):
         start_time, end_time = self.current_strategy.get_turn_start_end_time()
@@ -76,20 +86,16 @@ class CryptoTrade:
         self.is_running = True
 
         self.is_no_action_time = True
-        previous_cash = self.cash
+        self.previous_cash = self.cash
 
-        index = 0
+        self.backtest_index = 0
 
         while self.is_running:
             try:
                 running_now = datetime.datetime.now()
 
-                if crypto_param.exchange == "backtest":
-                    self.count += 1
-                    if self.count > 12:  # TODO : Set 1day simulation sample count(1~24)
-                        self.count = 0
-                        self.running_timing = False
-                        self.restart_timing = True
+                if self.is_backtest():
+                    self.backtest_set_trade_flag()
                 else:
                     self.running_timing = self.current_strategy.isRunningTiming(start_time, end_time, running_now)
                     self.restart_timing = self.current_strategy.isTurnRestartTiming(end_time, running_now)
@@ -105,67 +111,24 @@ class CryptoTrade:
                     # if not self.is_no_action_time:
                     self.current_strategy.execute_sell_strategy(self.remain_buy_list)
 
-                    if crypto_param.exchange == "backtest":
-                        self.quotation_api.set_sim_sub_index_update()
-                        index = self.quotation_api.get_sim_index_update() - self.init_back_index
-                        sub_index = self.quotation_api.get_sim_sub_index_update() - \
-                                    (self.quotation_api.get_sim_index_update() * 24)
-                        logger.debug("Simulation(" + str(index) + "-" + str(sub_index) + ")")
+                    if self.is_backtest():
+                        self.backtest_update_index()
 
                 elif self.restart_timing is True:
                     self.current_strategy.execute_turn_end_process()
 
                     # Update Next Turn Start/End Time
-                    # start_time, end_time = self.init_trade()
                     start_time, end_time = self.current_strategy.get_turn_start_end_time()
                     self.is_no_action_time = False
 
-                    if crypto_param.exchange == "backtest":
-                        running_now = self.trade_count + 1
-                        self.running_timing = True
-                        self.restart_timing = False
-                        self.quotation_api.set_sim_index_update()
-                        if (index - 1) >= self.sim_day_num:
-                            self.is_running = False
-                        else:
-                            logger.info("Simulation Turn Restart")
+                    if self.is_backtest():
+                        self.backtest_reset_parameter()
 
-                    self.cash = crypto_api.get_balance(self.exchange_api, "KRW")
-                    self.highest_cash = max(self.highest_cash, self.cash)
-                    self.lowest_cash = min(self.lowest_cash, self.cash)
-                    self.last_highest_cash = max(self.last_highest_cash, self.cash)
-                    self.last_lowest_cash = min(self.last_lowest_cash, self.cash)
-
-                    dd = -1
-
-                    if self.last_highest_cash == self.cash:
-                        self.last_lowest_cash = self.cash
-
-                    if self.last_lowest_cash == self.cash:
-                        dd = (self.last_highest_cash - self.last_lowest_cash) / self.last_highest_cash * 100.0
-                        self.mdd = max(self.mdd, dd)
-
-                    if self.mdd == dd:
-                        self.mdd_logs[str(running_now)] = {'mdd': self.mdd, 'last_highest': self.last_highest_cash,
-                                                           'last_lowest': self.last_lowest_cash}
-                    self.trade_logs[str(running_now)] = {'profit_ratio': (self.ror - 1) * 100.0 if self.ror > 1 else 0,
-                                                         'loss_ratio': (self.ror - 1) * 100.0 if self.ror < 1 else 0}
-
-                    self.trade_count += 1
-                    if previous_cash < self.cash:
-                        self.win_count += 1
-                    self.ror = self.cash / previous_cash
-                    self.accumulate_ror *= self.ror
-
-                    logger.info("****** " + str(self.cash) + " ******")
-
-                    previous_cash = self.cash
+                    self.update_trade_result()
 
                     self.is_no_action_time = True
                 else:
-                    sleep_time = 300
-                    logger.debug("No Action Time ~ " + str(sleep_time) + "s")
-                    time.sleep(sleep_time)
+                    self.process_no_action_time()
 
             except Exception as e:
                 logger.critical(e)
@@ -174,6 +137,45 @@ class CryptoTrade:
             time.sleep(self.transaction_time)
 
         logger.info("Stop " + self.current_strategy.name + " Trading")
+        self.print_trade_final_result()
+
+    def update_trade_result(self):
+        self.cash = crypto_api.get_balance(self.exchange_api, "KRW")
+        self.highest_cash = max(self.highest_cash, self.cash)
+        self.lowest_cash = min(self.lowest_cash, self.cash)
+        self.last_highest_cash = max(self.last_highest_cash, self.cash)
+        self.last_lowest_cash = min(self.last_lowest_cash, self.cash)
+
+        dd = -1
+
+        if self.last_highest_cash == self.cash:
+            self.last_lowest_cash = self.cash
+
+        if self.last_lowest_cash == self.cash:
+            dd = (self.last_highest_cash - self.last_lowest_cash) / self.last_highest_cash * 100.0
+            self.mdd = max(self.mdd, dd)
+
+        if self.mdd == dd:
+            self.mdd_logs[str(self.running_now)] = {'mdd': self.mdd, 'last_highest': self.last_highest_cash,
+                                               'last_lowest': self.last_lowest_cash}
+        self.trade_logs[str(self.running_now)] = {'profit_ratio': (self.ror - 1) * 100.0 if self.ror > 1 else 0,
+                                             'loss_ratio': (self.ror - 1) * 100.0 if self.ror < 1 else 0}
+
+        self.trade_count += 1
+        if self.previous_cash < self.cash:
+            self.win_count += 1
+        self.ror = self.cash / self.previous_cash
+        self.accumulate_ror *= self.ror
+
+        logger.info("****** " + str(self.cash) + " ******")
+
+        self.previous_cash = self.cash
+
+    def process_no_action_time(self):
+        logger.debug("No Action Time ~ " + str(self.no_action_sleep_time) + "s")
+        time.sleep(self.no_action_sleep_time)
+
+    def print_trade_final_result(self):
         logger.info("Trade Count : " + str(self.trade_count))
         logger.info("Win Count : " + str(self.win_count) + ", " + str(self.win_count / self.trade_count * 100.0) + "%")
         logger.info("ROR : " + str(self.ror))
@@ -194,3 +196,26 @@ class CryptoTrade:
 
         logger.debug(self.mdd_logs)
         logger.debug(self.trade_logs)
+
+    def backtest_update_index(self):
+        self.quotation_api.set_sim_sub_index_update()
+        self.backtest_index = self.quotation_api.get_sim_index_update() - self.init_back_index
+        sub_index = self.quotation_api.get_sim_sub_index_update() - (self.quotation_api.get_sim_index_update() * 24)
+        logger.debug("Simulation(" + str(self.backtest_index) + "-" + str(sub_index) + ")")
+
+    def backtest_set_trade_flag(self):
+        self.count += 1
+        if self.count > 12:  # TODO : Set 1day simulation sample count(1~24)
+            self.count = 0
+            self.running_timing = False
+            self.restart_timing = True
+
+    def backtest_reset_parameter(self):
+        self.running_now = self.trade_count + 1
+        self.running_timing = True
+        self.restart_timing = False
+        self.quotation_api.set_sim_index_update()
+        if (self.backtest_index - 1) >= self.sim_day_num:
+            self.is_running = False
+        else:
+            logger.info("Simulation Turn Restart")
